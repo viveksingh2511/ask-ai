fd512bbf0942b59010ea3acf6f008543b510d33a
unknown <viveksingheng9@gmail.com>
2025-05-12 22:36:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { URL } from './constants'
+
+export default function App() {
+const [question, setQuestion] = useState('');
+const [result, setResult] = useState(undefined)
+
+const payload ={
+  "contents": [{
+    "parts":[{"text": question}]
+    }]
+}
+const askQuestion = async() => {
+  let response = await fetch(URL,{
+    method:"POST",
+    body:JSON.stringify(payload)
+  });
+  response = await response.json();
+  setResult(response.candidates[0].content.parts[0].text);
+  
+}
+
+  return (
+    <div className='grid grid-cols-5 h-screen text-center'>
+      <div className='col-span-1 bg-zinc-800'>
+        
+      </div>
+      <div className='col-span-4 p-10'>
+        <div className='container h-110 overflow-auto'>
+          <div className='text-white '>
+            {result}
+          </div>
+        </div>
+        <div className='bg-zinc-800 w-1/2 p-1 pr-5 text-white m-auto rounded-4xl border border-zinc-700 flex h16'>
+          <input type='text' value={question} onChange={(event)=>setQuestion(event.target.value)} className='w-full h-full p-3 outline-none' placeholder='As me anything' />
+          <button onClick={askQuestion}>Ask</button>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyC3qXdwMTR8XdxUULccv8O47NztFt7sNEw"
